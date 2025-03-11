ba7299fc9fcad5cfdc52509f7ff18745bc6d3042
sashee <tamas.sallai@advancedweb.hu>
2025-03-11 11:58:45 +0100

    caching

--- clients/index.js
+++ clients/index.js
@@ -62,6 +62,9 @@ const readCredential = async (credentialName, envVarName) => {
 	}
 }
 
+const cacheDirectory = process.env.CACHE_DIRECTORY;
+console.log("cacheDirectory", cacheDirectory);
+
 const monitoringAwsSecretAccessKey = await readCredential("monitoring_aws_secret_access_key", "MONITORING_AWS_SECRET_ACCESS_KEY");
 const aws = new AwsClient({accessKeyId: process.env.MONITORING_AWS_ACCESS_KEY_ID, secretAccessKey: monitoringAwsSecretAccessKey});
 const sendMonitoring = async ({pathname, searchParams, method, body}) => {
@@ -96,12 +99,12 @@ const runCommand = async ({label, command, args, env, processStdout}) => {
 			maxBuffer: 5 * 1024 * 1024,
 			shell: runInShell,
 		});
-		console.log(JSON.stringify({stdout: (processStdout ?? ((val) => val))(stdout), stderr}));
+		console.log(label, JSON.stringify({stdout: (processStdout ?? ((val) => val))(stdout), stderr}));
 		await sendMonitoring({pathname: "/log", searchParams: {runid: runId, label}, method: "POST", body: JSON.stringify({stdout: (processStdout ?? ((val) => val))(stdout), stderr})}).catch((e) => console.error(e));
 	}catch(e) {
 		if (e.stdout !== undefined && e.stderr !== undefined) {
 			// error is thrown by the execFile
-			console.log(e.stdout, e.stderr);
+			console.log(label, e.stdout, e.stderr);
 			await sendMonitoring({pathname: "/log", searchParams: {runid: runId, label: `${label}-error`}, method: "POST", body: JSON.stringify({stdout: e.stdout, stderr: e.stderr})}).catch((e) => console.error(e));
 			throw e;
 		}else {
@@ -116,14 +119,33 @@ try {
 	const resticPassword = await readCredential("restic_password", "RESTIC_PASSWORD");
 	const packageJsonVersion = JSON.parse(await fs.readFile(new URL("./package.json", import.meta.url), "utf8")).version;
 
-	await runCommand({label: "version", command: "restic", args: ["version"], env: {}, processStdout: (stdout) => {
-		return {
-			resticVersion: stdout,
-			packageVersion: packageJsonVersion,
-			nodeVersion: process.version,
-		};
-	}});
-	await runCommand({label: "unlock", command: "restic", args: ["unlock"], env: {AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID, RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY, AWS_SECRET_ACCESS_KEY: awsSecretAccessKey, RESTIC_PASSWORD: resticPassword}});
+	await runCommand({
+		label: "version",
+		command: "restic",
+		args: ["version"],
+		env: {
+			RESTIC_CACHE_DIR: cacheDirectory,
+		},
+		processStdout: (stdout) => {
+			return {
+				resticVersion: stdout,
+				packageVersion: packageJsonVersion,
+				nodeVersion: process.version,
+			};
+		},
+	});
+	await runCommand({
+		label: "unlock", 
+		command: "restic",
+		args: ["unlock"],
+		env: {
+			AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+			RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY,
+			AWS_SECRET_ACCESS_KEY: awsSecretAccessKey,
+			RESTIC_PASSWORD: resticPassword,
+			RESTIC_CACHE_DIR: cacheDirectory,
+		},
+	});
 	await runCommand({
 		label: "backup",
 		command: "restic",
@@ -134,6 +156,7 @@ try {
 			AWS_SECRET_ACCESS_KEY: awsSecretAccessKey,
 			RESTIC_PASSWORD: resticPassword,
 			RESTIC_PROGRESS_FPS: "0.01",
+			RESTIC_CACHE_DIR: cacheDirectory,
 		},
 		processStdout: (stdout) => [stdout.split("\n").findLast((line) => isJson(line))].map((line) => JSON.parse(line))[0],
 	});
@@ -146,7 +169,8 @@ try {
 				AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
 				RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY,
 				AWS_SECRET_ACCESS_KEY: awsSecretAccessKey,
-				RESTIC_PASSWORD: resticPassword
+				RESTIC_PASSWORD: resticPassword,
+				RESTIC_CACHE_DIR: cacheDirectory,
 			},
 			// prune does not support JSON yet
 			// https://restic.readthedocs.io/en/stable/075_scripting.html
@@ -157,9 +181,44 @@ try {
 			throw e;
 		}
 	}
-	await runCommand({label: "check", command: "restic", args: ["check"], env: {AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID, RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY, AWS_SECRET_ACCESS_KEY: awsSecretAccessKey, RESTIC_PASSWORD: resticPassword}});
-	await runCommand({label: "snapshots", command: "restic", args: ["snapshots", "--json"], env: {AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID, RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY, AWS_SECRET_ACCESS_KEY: awsSecretAccessKey, RESTIC_PASSWORD: resticPassword}, processStdout: (val) => JSON.parse(val)});
-	await runCommand({label: "stats", command: "restic", args: ["stats", "latest", "--json"], env: {AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID, RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY, AWS_SECRET_ACCESS_KEY: awsSecretAccessKey, RESTIC_PASSWORD: resticPassword}, processStdout: (val) => JSON.parse(val)});
+	await runCommand({
+		label: "check",
+		command: "restic",
+		args: ["check"],
+		env: {
+			AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+			RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY,
+			AWS_SECRET_ACCESS_KEY: awsSecretAccessKey,
+			RESTIC_PASSWORD: resticPassword,
+			RESTIC_CACHE_DIR: cacheDirectory,
+		},
+	});
+	await runCommand({
+		label: "snapshots",
+		command: "restic",
+		args: ["snapshots", "--json"],
+		env: {
+			AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+			RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY,
+			AWS_SECRET_ACCESS_KEY: awsSecretAccessKey,
+			RESTIC_PASSWORD: resticPassword,
+			RESTIC_CACHE_DIR: cacheDirectory,
+		},
+		processStdout: (val) => JSON.parse(val),
+	});
+	await runCommand({
+		label: "stats",
+		command: "restic",
+		args: ["stats", "latest", "--json"],
+		env: {
+			AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+			RESTIC_REPOSITORY: process.env.RESTIC_REPOSITORY,
+			AWS_SECRET_ACCESS_KEY: awsSecretAccessKey,
+			RESTIC_PASSWORD: resticPassword,
+			RESTIC_CACHE_DIR: cacheDirectory,
+		},
+		processStdout: (val) => JSON.parse(val),
+	});
 
 	await sendMonitoring({pathname: "/success", searchParams: {runid: runId}, method: "GET"});
 }catch(e) {
